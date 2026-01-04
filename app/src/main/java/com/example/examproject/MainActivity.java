55496508a5e0a1c361f3c8e7edabf51ea908df61
marwan1203 <marwanhigazi5#@gmail.com>
2026-01-05 01:04:13 +0200

    ,

---
+++ app/src/main/java/com/example/examproject/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.examproject;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+TextView siteName,enterView,siteType;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        }
+    }
